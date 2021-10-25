8c219bf0b3cb8de3b46a961aaf87159a0c31be23
Maksim Shylau <shilaums@gmail.com>
2021-10-25 19:38:30 +0300

    feat: replace ebs route with API Gateway proxy

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -3,5 +3,5 @@ export const API_PATHS = {
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://hifx2vr6f4.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://icqa4iybt4.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'http://alreadybored-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+	cart: 'https://ffdwv0zusg.execute-api.eu-west-1.amazonaws.com/default/api',
 };
